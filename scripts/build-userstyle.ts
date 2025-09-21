681fcef11a234c04f95b0138b3933d95e7cdd4de
WalkQuackBack <quacksareback@duck.com>
2025-09-20 21:00:57 -0400

    feat: expand to sheets, slides

--- scripts/build-userstyle.ts
+++ scripts/build-userstyle.ts
@@ -3,14 +3,22 @@ import { readFile, writeFile } from "fs/promises";
 
 import { format } from "prettier";
 
-const template: string = await readFile('./template.css', 'utf8');
-
 const TEMPLATE_REPLACE_STRING = '/**** Generated code REPLACE ****/'
 
-async function main() {
-    const stylesheet = await dumpStylesheets([
+const buildList = {
+    "build": [
         "https://docs.google.com/document/d/1RDErYoVPRCvy2nRvWo8a1xa5m7NrxpWBzZirE97m_3g/"
-    ])
+    ],
+    "slides": [
+        "https://docs.google.com/presentation/d/1-jVYOX5SuCT9hU9sOKgPEMOJ5W6X3bWD5SnMm7Kz6Sc/"
+    ],
+    "sheets": [
+        "https://docs.google.com/spreadsheets/d/193-MPXvRvu9rCsRXsXHCoeMxuXmSF8jcZw9Er0EaVSo/"
+    ],
+}
+
+async function buildSite(id: string, sites: string[]) {
+    const stylesheet = await dumpStylesheets(sites)
     console.log('Templating stylesheets...')
 
     const date = new Date()
@@ -18,14 +26,24 @@ async function main() {
     const month = date.getUTCMonth() + 1;
     const day = date.getUTCDate();
 
+    const template: string = await readFile(`./templates/${id}.css`, 'utf8');
+
     const output = await format(
         template.replace(TEMPLATE_REPLACE_STRING, stylesheet)
             .replace('<version>', `${year}.${month}.${day}`),
         { parser: "css" }
     )
-    console.log('Writing output...')
-    await writeFile('build.user.css', output)
-    console.log('Wrote output')
+    return output
+}
+
+async function main() {
+    for (const [id, sites] of Object.entries(buildList)) {
+        console.log('Building site ' + sites + ' to ' + id)
+        const output = await buildSite(id, sites)
+        console.log('Writing output...')
+        await writeFile(id + '.user.css', output)
+        console.log('Wrote output')
+    }
 }
 
 main()
